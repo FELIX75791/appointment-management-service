50b78b02605abeb143b3cb74ebfb01019ec021c9
Ziyue Jin <ziyuejin877@gmail.com>
2024-10-18 18:38:27 -0400

    feat: minor fix on registerClient endpoint

--- appointment-management/src/main/java/org/dljl/controller/ClientController.java
+++ appointment-management/src/main/java/org/dljl/controller/ClientController.java
@@ -9,10 +9,23 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * Controller class that handles client registration and generates a unique client ID. Provides an
+ * endpoint to register a client and return a unique client ID along with a message. Clients using
+ * this service is responsible to save generated id.
+ */
 @RestController
 @RequestMapping("clients")
 public class ClientController {
 
+  /**
+   * Endpoint to register a new client. This endpoint generates a unique client ID using UUID and
+   * returns it in the response. It also provides a message instructing the client to save the
+   * generated ID.
+   *
+   * @return ResponseEntity containing a map with the generated client ID and an advisory message.
+   *     The HTTP status code for the response is 201 (Created).
+   */
   @PostMapping("/registerClient")
   public ResponseEntity<Map<String, String>> registerClient() {
     // Generate UUID for the client ID
@@ -21,7 +34,10 @@ public class ClientController {
     // Prepare response with client ID and message
     Map<String, String> response = new HashMap<>();
     response.put("client_id", clientId);
-    response.put("message", "SAVE YOUR CLIENT ID. LOSING IT WILL POTENTIALLY LOSE PREVIOUS INFO AND REQUIRE YOU TO REGENERATE ONE");
+    response.put(
+        "message",
+        "SAVE YOUR CLIENT ID. "
+            + "LOSING IT WILL POTENTIALLY LOSE PREVIOUS INFO AND REQUIRE YOU TO REGENERATE ONE");
 
     return ResponseEntity.status(HttpStatus.CREATED).body(response);
   }
